df5410fcae5ad884eb216d818dd5270de98ad27a
Chris Koerner <chris.koerner@e-hps.com>
2015-10-06 09:21:02 -0400

    Quote fix
    Fix the quotes.

--- classes/class-wc-gateway-securesubmit.php
+++ classes/class-wc-gateway-securesubmit.php
@@ -303,7 +303,7 @@ class WC_Gateway_SecureSubmit extends WC_Payment_Gateway
                 if ($this->email_fraud == 'yes' && $this->fraud_address != '') {
                     wc_mail(
                         $this->fraud_address,
-                        'Suspicious order ' . ($this->allow_fraud == 'yes' ? 'allowed' : 'declined') . ' ' . (' . $order_id . '),
+                        'Suspicious order ' . ($this->allow_fraud == 'yes' ? 'allowed' : 'declined') . ' (' . $order_id . ')',
                         'Hello,<br><br>Heartland has determined that you should review order ' . $order_id . ' for the amount of ' . $order->order_total . '.<p><br></p>'.
                         '<p>You have received this email because you have configured the \'Email store owner on suspicious orders\' settings in the [WooCommerce | Checkout | SecureSubmit] options page.</p>'
                     );
