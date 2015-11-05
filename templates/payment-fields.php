8d1d6349abcad4ea423063acd577309c9d4eaf2d
stephanie-clark <szuroff@gmail.com>
2015-11-05 13:50:33 -0500

    Put "Pay with New Card" input fields into container

--- templates/payment-fields.php
+++ templates/payment-fields.php
@@ -4,6 +4,8 @@
 </div>
 
 <fieldset>
+  
+   <!-- Start Description -->
    <?php if ($this->description): ?>
       <div class="securesubmit-content">
          <p class="securesubmit-description">
@@ -12,9 +14,14 @@
       </div>
       <hr />
    <?php endif; ?>
+   <!-- End Description -->
+       
+   <!-- Start "if card saving is allowed" -->
    <?php if ($this->allow_card_saving): ?>
-      <div class="securesubmit-content">
-         <?php $cards = get_user_meta(get_current_user_id(), '_secure_submit_card', false); ?>
+   <div class="securesubmit-content">
+              
+      <!-- Start LOGGED IN SAVED CARDS -->
+      <?php $cards = get_user_meta(get_current_user_id(), '_secure_submit_card', false); ?>
          <?php if (is_user_logged_in() && isset($cards)): ?>
             <div class="saved-creditcards-list-header saved-creditcards-list form-row form-row-wide no-bottom-margin no-bottom-padding">
                <div class="ss-section-header clearfix">
@@ -24,15 +31,19 @@
                   </a>
                </div>
             </div>
+      
+            <!-- START SAVED CREDIT CARD LIST -->        
             <div class="saved-creditcards-list form-row form-row-wide no-bottom-margin">
-            <?php
-               $checked = ' checked="checked" ';
-               $totalcards = 0;
-            ?>
+               <?php
+                  $checked = ' checked="checked" ';
+                  $totalcards = 0;
+               ?>
                <?php foreach ($cards as $i => $card): ?>
                   <div class="saved-card saved-card-<?php echo strtolower($card['card_type']); ?><?php echo ($checked != '' ? ' active' : '');?>">
                      <div class="saved-card-selector">
-                        <input <?php echo $checked; ?> class="saved-selector" type="radio" id="secure_submit_card_<?php echo $i; ?>" name="secure_submit_card" style="width:auto;" value="<?php echo $i; ?>" />
+                        <input <?php echo $checked; ?> class="saved-selector" type="radio" id="secure_submit_card_
+                        <?php echo $i; ?>" name="secure_submit_card" style="width:auto;" value="
+                        <?php echo $i; ?>" />
                      </div>
                      <div class="saved-card-info">
                         <label for="secure_submit_card_<?php echo $i; ?>">
@@ -43,28 +54,32 @@
                            <p><span>Expires on <?php echo $card['exp_month'] . '/' . $card['exp_year']; ?></span></p>
                         </label>
                      </div>
-                  </div>
+                  </div>          
                   <?php $checked = ""; ?>
-               <?php endforeach; ?>
-                <div class="saved-card saved-card-new<?php echo ($checked != '' ? ' active' : '');?>">
-                   <div class="saved-card-selector">
-                      <input <?php echo $checked; ?> type="radio" class="saved-selector" id="secure_submit_card_new" name="secure_submit_card" style="width:auto;" value="new" />
-                   </div>
-                   <div class="saved-card-info-new">
-                      <label for="secure_submit_card_new">
-                         <p>
-                            Pay with a new credit card
-                         </p>
-                      </label>
-                   </div>
-                </div>
-            </div>
-            <div class="clear"></div>
-         <?php endif; ?>
-      </div>
-   <?php endif; ?>
+                  <?php endforeach; ?>
 
-   <?php
+
+                 <!-- START NEW CARD -->
+                  <div class="saved-card saved-card-new<?php echo ($checked != '' ? ' active' : '');?>">
+                     <div class="saved-card-selector">
+                        <input <?php echo $checked; ?> type="radio" class="saved-selector" id="secure_submit_card_new" name="secure_submit_card" style="width:auto;" value="new" />
+                     </div>
+                     <div class="saved-card-info-new">
+                        <label for="secure_submit_card_new">
+                           <p>
+                              Pay with a new credit card
+                           </p>
+                        </label>
+                     </div>
+                  </div>
+                     </div>
+                     <div class="clear"></div>
+                                    
+         <?php endif; ?>
+                                     </div>
+         <?php endif; ?>
+
+                                          <?php
    if ($checked = " checked ") {
       $styletag = "display:none;";
    } else {
@@ -72,70 +87,75 @@
    }
    ?>
 
-   <div class="securesubmit-content new-card-content" style="<?php echo $styletag; ?>">
-      <div class="securesubmit_new_card">
-         <div class="securesubmit_new_card_info">
-            <div class="form-row form-row-wide no-bottom-margin no-bottom-padding hideable">
-               <label for="securesubmit_card_number">
-                  <?php _e("Credit Card number", 'wc_securesubmit') ?>
-                  <span class="required">*</span>
-               </label>
-               <?php if ($this->use_iframes): ?>
-                  <div id="securesubmit_card_number"></div>
-               <?php else: ?>
-                  <input id="securesubmit_card_number" type="tel" autocomplete="off" class="input-text card-number" placeholder="•••• •••• •••• ••••" />
-               <?php endif; ?>
-            </div>
-            <div class="clear"></div>
-            <div class="form-row hideable no-bottom-margin">
-               <div class="form-row-first half-row">
-                  <label for="securesubmit_card_expiration">
-                     <?php _e("Expiration date", 'wc_securesubmit') ?>
-                     <span class="required">*</span>
-                  </label>
-                  <?php if ($this->use_iframes): ?>
-                     <div id="securesubmit_card_expiration"></div>
-                  <?php else: ?>
-                     <input id="securesubmit_card_expiration" type="tel" autocomplete="off" class="input-text expiry-date" placeholder="MM / YYYY" />
-                  <?php endif; ?>
-               </div>
-               <div class="form-row-last half-row">
-                  <label for="securesubmit_card_cvv">
-                     <?php _e("Security code", 'wc_securesubmit') ?>
-                     <span class="required">*</span>
-                  </label>
-                  <?php if ($this->use_iframes): ?>
-                     <div id="securesubmit_card_cvv"></div>
-                  <?php else: ?>
-                     <input type="tel" id="securesubmit_card_cvv" maxlength="4" autocomplete="off" class="input-text card-cvc" placeholder="CVV" />
-                  <?php endif; ?>
-                  <span class="help securesubmit_card_csc_description"></span>
-               </div>
-            </div>
-         </div>
-         <?php if ($this->allow_card_saving == 'yes'): ?>
-            <div class="form-row form-row-wide no-top-margin no-top-padding no-bottom-paddig no-bottom-margin">
-               <p class="form-row form-row-wide securesubmit-save-cards">
-                  <input type="checkbox" autocomplete="off" id="save_card" name="save_card" value="true" style="display:inline">
-                  <label for="save_card" style="display: inline;">
-                     <?php _e("Save Credit Card for Future Use", 'wc_securesubmit') ?>
-                  </label>
-               </div>
-            </p>
-         <?php endif; ?>
-         <div class="clear"></div>
+                                             <div class="securesubmit-content new-card-content" style="<?php echo $styletag; ?>">
+                                                <div class="securesubmit_new_card">
+                                                   <div class="securesubmit_new_card_info">
+                                                      <div class="form-row form-row-wide no-bottom-margin no-bottom-padding hideable">
+                                                         <label for="securesubmit_card_number">
+                                                            <?php _e("Credit Card number", 'wc_securesubmit') ?>
+                                                               <span class="required">*</span>
+                                                         </label>
+                                                         <?php if ($this->use_iframes): ?>
+                                                            <div id="securesubmit_card_number"></div>
+                                                            <?php else: ?>
+                                                               <input id="securesubmit_card_number" type="tel" autocomplete="off" class="input-text card-number" placeholder="•••• •••• •••• ••••" />
+                                                               <?php endif; ?>
+                                                      </div>
+                                                      <div class="clear"></div>
+                                                      <div class="form-row hideable no-bottom-margin">
+                                                         <div class="form-row-first half-row">
+                                                            <label for="securesubmit_card_expiration">
+                                                               <?php _e("Expiration date", 'wc_securesubmit') ?>
+                                                                  <span class="required">*</span>
+                                                            </label>
+                                                            <?php if ($this->use_iframes): ?>
+                                                               <div id="securesubmit_card_expiration"></div>
+                                                               <?php else: ?>
+                                                                  <input id="securesubmit_card_expiration" type="tel" autocomplete="off" class="input-text expiry-date" placeholder="MM / YYYY" />
+                                                                  <?php endif; ?>
+                                                         </div>
+                                                         <div class="form-row-last half-row">
+                                                            <label for="securesubmit_card_cvv">
+                                                               <?php _e("Security code", 'wc_securesubmit') ?>
+                                                                  <span class="required">*</span>
+                                                            </label>
+                                                            <?php if ($this->use_iframes): ?>
+                                                               <div id="securesubmit_card_cvv"></div>
+                                                               <?php else: ?>
+                                                                  <input type="tel" id="securesubmit_card_cvv" maxlength="4" autocomplete="off" class="input-text card-cvc" placeholder="CVV" />
+                                                                  <?php endif; ?>
+                                                                     <span class="help securesubmit_card_csc_description"></span>
+                                                         </div>
+                                                      </div>
 
-      </div>
-   </div>
+                                                      <?php if ($this->allow_card_saving == 'yes'): ?>
+                                                         <div class="form-row form-row-wide no-top-margin no-top-padding no-bottom-paddig no-bottom-margin">
+                                                            <p class="form-row form-row-wide securesubmit-save-cards">
+                                                               <input type="checkbox" autocomplete="off" id="save_card" name="save_card" value="true" style="display:inline">
+                                                               <label for="save_card" style="display: inline;">
+                                                                  <?php _e("Save Credit Card for Future Use", 'wc_securesubmit') ?>
+                                                               </label>
+                                                         </div>
+                                                         </p>
+                                                         <?php endif; ?>
+                                                   </div>
+
+
+                                                   <div class="clear"></div>
+
+                                                </div>
+                                             </div>
+
+
 </fieldset>
 <?php if ($this->use_iframes): // Create the iframes when WC refreshes the payment fields ?>
    <script>
       window.securesubmitLoadIframes = window.securesubmitLoadIframes || function () {};
       window.securesubmitLoadIframes();
    </script>
-<?php endif; ?>
-<?php // Attach the field event handlers when WC refreshes the payment fields ?>
-<script>
-   window.securesubmitLoadEvents = window.securesubmitLoadEvents || function () {};
-   window.securesubmitLoadEvents();
-</script>
+   <?php endif; ?>
+      <?php // Attach the field event handlers when WC refreshes the payment fields ?>
+         <script>
+            window.securesubmitLoadEvents = window.securesubmitLoadEvents || function () {};
+            window.securesubmitLoadEvents();
+         </script>
