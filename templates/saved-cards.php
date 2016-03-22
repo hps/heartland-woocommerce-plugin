74f8009d0aa88bd1a3da2b878acb49a3b057ec7f
Ty Goss <tyler.goss@e-hps.com>
2016-03-22 15:45:50 -0400

    iframe and saved cards styling
    Added some CSS to help prevent iframes from getting their opacity
    messed with.
    
    Also styled the saved cards on the account page to look like the saved
    cards on the checkout page.

--- templates/saved-cards.php
+++ templates/saved-cards.php
@@ -1,28 +1,30 @@
 <h2>Saved Credit Cards</h2>
-<table>
-    <thead>
-        <tr>
-            <th>Card Type</th>
-            <th>Last Four</th>
-            <th>Expiration</th>
-            <th>&nbsp;</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php $cards = get_user_meta(get_current_user_id(), '_secure_submit_card', false); ?>
-        <?php foreach ($cards as $i => $card): ?>
-        <tr>
-            <td><?php esc_html_e($card['card_type']); ?></td>
-            <td><?php esc_html_e($card['last_four']); ?></td>
-            <td><?php esc_html_e($card['exp_month']); ?> / <?php esc_html_e($card['exp_year']); ?></td>
-            <td>
-                <form action="#saved-cards" method="POST">
-                    <?php wp_nonce_field('secure_submit_del_card'); ?>
-                    <input type="hidden" name="delete_card" value="<?php esc_attr($i); ?>">
-                    <input type="submit" value="Delete Card">
-                </form>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+
+<div class="saved-creditcards-list form-row form-row-wide account-saved-cards-list">
+    
+    <?php $cards = get_user_meta(get_current_user_id(), '_secure_submit_card', false); ?>
+    
+    <?php foreach ($cards as $i => $card): ?>
+
+        <div class="clearfix saved-card saved-card-<?php esc_html_e( strtolower($card['card_type']) ); ?>">
+            <div class="account-saved-card-logo">
+                <div class="card-type-logo"></div>
+            </div>
+            <div class="saved-card-info">
+                <label for="secure_submit_card_0">
+                    <p><?php esc_html_e($card['card_type']); ?> ending in <?php esc_html_e($card['last_four']); ?></p>
+                    <p><span>Expires on <?php esc_html_e($card['exp_month']); ?>/<?php esc_html_e($card['exp_year']); ?></span></p>
+                </label>
+            </div>
+            
+            <form action="#saved-cards" method="POST">
+                <?php wp_nonce_field('secure_submit_del_card'); ?>
+                <input type="hidden" name="delete_card" value="<?php esc_attr($i); ?>">
+                <input type="submit" value="Delete Card">
+            </form>
+           
+        </div>
+    
+    <?php endforeach; ?>
+    
+</div>
