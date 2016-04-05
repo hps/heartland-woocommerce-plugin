27fa365c4149f7232a27c491c6e59b5d5d31cd14
Chris Koerner <chris.koerner@e-hps.com>
2016-04-05 06:50:01 -0400

    Update text translation key

--- classes/includes/settings-paypal.php
+++ classes/includes/settings-paypal.php
@@ -9,61 +9,61 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 return array(
 	'enabled' => array(
-		'title'   => __( 'Enable/Disable', 'woocommerce' ),
+		'title'   => __( 'Enable/Disable', 'wc_securesubmit' ),
 		'type'    => 'checkbox',
-		'label'   => __( 'Enable PayPal Secure Submit', 'woocommerce' ),
+		'label'   => __( 'Enable PayPal Secure Submit', 'wc_securesubmit' ),
 		'default' => 'yes'
 	),
 	'title' => array(
-		'title'       => __( 'Title', 'woocommerce' ),
+		'title'       => __( 'Title', 'wc_securesubmit' ),
 		'type'        => 'text',
-		'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-		'default'     => __( 'PayPal', 'woocommerce' ),
+		'description' => __( 'This controls the title which the user sees during checkout.', 'wc_securesubmit' ),
+		'default'     => __( 'PayPal', 'wc_securesubmit' ),
 		'desc_tip'    => true,
 	),
 	'description' => array(
-		'title'       => __( 'Description', 'woocommerce' ),
+		'title'       => __( 'Description', 'wc_securesubmit' ),
 		'type'        => 'text',
 		'desc_tip'    => true,
-		'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
-		'default'     => __( 'Pay via PayPal; you can pay with your credit card if you don\'t have a PayPal account.', 'woocommerce' )
+		'description' => __( 'This controls the description which the user sees during checkout.', 'wc_securesubmit' ),
+		'default'     => __( 'Pay via PayPal; you can pay with your credit card if you don\'t have a PayPal account.', 'wc_securesubmit' )
 	),
 	'testmode' => array(
-		'title'       => __( 'PayPal Sandbox', 'woocommerce' ),
+		'title'       => __( 'PayPal Sandbox', 'wc_securesubmit' ),
 		'type'        => 'checkbox',
-		'label'       => __( 'Enable PayPal sandbox', 'woocommerce' ),
+		'label'       => __( 'Enable PayPal sandbox', 'wc_securesubmit' ),
 		'default'     => 'no',
-		'description' => sprintf( __( 'PayPal sandbox can be used to test payments. Sign up for a developer account <a href="%s">here</a>.', 'woocommerce' ), 'https://developer.paypal.com/' ),
+		'description' => sprintf( __( 'PayPal sandbox can be used to test payments. Sign up for a developer account <a href="%s">here</a>.', 'wc_securesubmit' ), 'https://developer.paypal.com/' ),
 	),
 	'debug' => array(
-		'title'       => __( 'Debug Log', 'woocommerce' ),
+		'title'       => __( 'Debug Log', 'wc_securesubmit' ),
 		'type'        => 'checkbox',
-		'label'       => __( 'Enable logging', 'woocommerce' ),
+		'label'       => __( 'Enable logging', 'wc_securesubmit' ),
 		'default'     => 'no',
-		'description' => sprintf( __( 'Adds error logging information to the following log file :  <code>%s</code>', 'woocommerce' ), wc_get_log_file_path( 'paypal' ) )
+		'description' => sprintf( __( 'Adds error logging information to the following log file :  <code>%s</code>', 'wc_securesubmit' ), wc_get_log_file_path( 'paypal' ) )
 	),
 	'paymentaction' => array(
-		'title'       => __( 'Payment Action', 'woocommerce' ),
+		'title'       => __( 'Payment Action', 'wc_securesubmit' ),
 		'type'        => 'select',
 		'class'       => 'wc-enhanced-select',
-		'description' => __( 'Choose whether you wish to capture funds immediately or authorize payment only.', 'woocommerce' ),
+		'description' => __( 'Choose whether you wish to capture funds immediately or authorize payment only.', 'wc_securesubmit' ),
 		'default'     => 'sale',
 		'desc_tip'    => true,
 		'options'     => array(
-			'sale'          => __( 'Capture', 'woocommerce' ),
-			'authorization' => __( 'Authorize', 'woocommerce' )
+			'sale'          => __( 'Capture', 'wc_securesubmit' ),
+			'authorization' => __( 'Authorize', 'wc_securesubmit' )
 		)
 	),
     'public_key' => array(
-	    'title'       => __( 'Public Key', 'woocommerce' ),
+	    'title'       => __( 'Public Key', 'wc_securesubmit' ),
 	    'type'        => 'text',
-	    'description' => __( 'Get your API keys from your SecureSubmit account.', 'woocommerce' ),
+	    'description' => __( 'Get your API keys from your SecureSubmit account.', 'wc_securesubmit' ),
 	    'default'     => ''
     ),
     'secret_key' => array(
-	    'title'       => __( 'Secret Key', 'woocommerce' ),
+	    'title'       => __( 'Secret Key', 'wc_securesubmit' ),
 	    'type'        => 'text',
-	    'description' => __( 'Get your API keys from your SecureSubmit account.', 'woocommerce' ),
+	    'description' => __( 'Get your API keys from your SecureSubmit account.', 'wc_securesubmit' ),
 	    'default'     => ''
     ),
 );
