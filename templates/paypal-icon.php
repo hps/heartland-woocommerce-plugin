acd3deddaed865937cc5082d0f9eb662b615a67d
Shane Logsdon <shane@shanelogsdon.com>
2017-01-30 10:59:17 -0500

    correct quoting

--- templates/paypal-icon.php
+++ templates/paypal-icon.php
@@ -2,7 +2,7 @@
      alt="<?php echo __('PayPal Acceptance Mark', 'wc_securesubmit'); ?>" />
 <?php $url = esc_url('https://www.paypal.com/us/webapps/mpp/paypal-popup'); ?>
 <a href="<?php echo $url; ?>" class="about_paypal"
-   onclick="javascript:window.open(\'<?php echo $url; ?>\',\'WIPaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700\'); return false;"
+   onclick="javascript:window.open('<?php echo $url; ?>','WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"
    title="<?php echo esc_attr__('What is PayPal?', 'wc_securesubmit'); ?>">
     <?php echo esc_attr__('What is PayPal?', 'wc_securesubmit'); ?>
 </a>
