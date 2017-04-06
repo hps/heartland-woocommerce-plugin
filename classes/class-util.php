600c43726d81f1d858ab56071f703db080f72c79
Shane Logsdon <shane@shanelogsdon.com>
2017-04-06 14:58:28 -0400

    add shared utility class

---
+++ classes/class-util.php
@@ -0,0 +1,28 @@
+<?php
+
+class WC_SecureSubmit_Util
+{
+    /**
+     * Wraps WooCommerce data access APIs to ensure proper data is retrieved
+     * without the merchant experiencing issues during the WooCommerce
+     * 2.6 -> 3.0 transition.
+     *
+     * @param object $object   Data object to test
+     * @param string $method   Method name for 3.0 API
+     * @param string $property Property name for 2.6 API
+     *
+     * @return mixed
+     */
+    public static function getData($object, $method, $property)
+    {
+        if (method_exists($object, $method)) {
+            return $object->{$method}();
+        }
+
+        if (property_exists($object, $property)) {
+            return $object->{$property};
+        }
+
+        return null;
+    }
+}
