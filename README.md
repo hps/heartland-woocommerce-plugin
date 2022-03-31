4c6e0bf78017e3442361062556f5641eac9db407
Tony Smedal <msmedalj@gmail.com>
2022-03-31 10:06:21 -0400

    address CVE-2017-9841

--- README.md
+++ README.md
@@ -20,6 +20,10 @@ Download the contents and extract to your WordPress plugin folder. Activate.
 
 ## Changelog
 
+#### 1.13.1
+
+* Security patch
+
 #### 1.13.0
 
 * Add filters for third-party fraud solutions
--- gateway-securesubmit.php
+++ gateway-securesubmit.php
@@ -3,8 +3,8 @@
 Plugin Name: WooCommerce SecureSubmit Gateway
 Plugin URI: https://developer.heartlandpaymentsystems.com/SecureSubmit/
 Description: Heartland Payment Systems gateway for WooCommerce.
-Version: 1.13.0
-WC tested up to: 4.5.2
+Version: 1.13.1
+WC tested up to: 6.3.1
 Author: SecureSubmit
 Author URI: https://developer.heartlandpaymentsystems.com/SecureSubmit/
 */
