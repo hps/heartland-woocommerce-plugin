6e18e1bd5a5c83dcdb37b439e4c4c4f0266d71c1
Tim Flight <4959691+tflight@users.noreply.github.com>
2023-09-21 13:24:25 -0400

    check for exact legnth

--- templates/payment-fields.php
+++ templates/payment-fields.php
@@ -156,7 +156,7 @@ if ($this->allow_gift_cards && $gift_cards_allowed) : // Allow customers to pay 
                             checkEnableGiftCardApplyButton();
                           });
                           function checkEnableGiftCardApplyButton() {
-                            if (jQuery("#gift-card-number").val().length >= 16 && jQuery("#gift-card-pin").val().length >= 4) {
+                            if (jQuery("#gift-card-number").val().length === 19 && jQuery("#gift-card-pin").val().length === 4) {
                                 jQuery("#apply-gift-card").css("visibility", "visible");
                             } else {
                                 jQuery("#apply-gift-card").css("visibility", "hidden");
