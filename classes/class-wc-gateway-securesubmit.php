62c585956c49b24f28e3857f9f1bb038aa3a9c66
Tony Smedal <msmedalj@gmail.com>
2025-04-09 19:45:27 -0400

    v2.3.5 prerelease notes

--- classes/class-wc-gateway-securesubmit.php
+++ classes/class-wc-gateway-securesubmit.php
@@ -10,7 +10,7 @@ class WC_Gateway_SecureSubmit extends WC_Payment_Gateway
     public $payment = null;
     public $refund  = null;
     public $reverse = null;
-    private $pluginVersion = '2.3.4';
+    private $pluginVersion = '2.3.5';
     public $secret_key;
     public $public_key;
     public $custom_error;
